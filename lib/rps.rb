d1da66ddde5bca288130690e166b239334009dcb
Katie Fujihara and Masen Matthews <katie@>
2018-02-07 11:12:04 -0800

    refactor to use is and elsif statements

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,32 +1,25 @@
 class Rps
-  def rock_wins?(input1, input2)
+  def wins?(input1, input2)
     if (input1 == "rock"||input1 == "scissors") && (input2 == "scissors"||input2 == "rock")
-      true
+      "rock wins"
+    elsif (input1 == "scissors"||input1 == "paper") && (input2 == "paper"||input2 == "scissors")
+     "scissors wins"
+    elsif (input1 == "paper" || input1 == "rock") && (input2 == "rock" || input2 == "paper")
+      "paper wins"
     end
   end
-  def scissors_wins?(input1, input2)
-    if (input1 == "scissors"||input1 == "paper") && (input2 == "paper"||input2 == "scissors")
-      true
-    end
-  end
-  def paper_wins?(input1, input2)
-    if (input1 == "paper" || input1 == "rock") && (input2 == "rock" || input2 == "paper")
-      true
-    end
-  end
-  def rock_loses?(input1, input2)
+
+  def loses?(input1, input2)
     if (input1 == "rock"||input1 == "paper") && (input2 == "paper"||input2 == "rock")
-      true
+      "rock loses"
+    elsif (input1 == "scissors"||input1 == "rock") && (input2 == "rock"||input2 == "scissors")
+      "scissors loses"
+    elsif (input1 == "scissors"||input1 == "paper") && (input2 == "paper"||input2 == "scissors")
+      "paper loses"
     end
   end
-  def scissors_loses?(input1, input2)
-    if (input1 == "scissors"||input1 == "rock") && (input2 == "rock"||input2 == "scissors")
-      true
-    end
-  end
-  def paper_loses?(input1, input2)
-    if (input1 == "scissors"||input1 == "paper") && (input2 == "paper"||input2 == "scissors")
-      true
-    end
-  end
+  puts 'choose your weapon'
+  input1 = gets.chomp
+  puts 'choose weapon 2'
+  input2 = gets.chomp
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -5,26 +5,26 @@ require('pry')
 describe('#rps') do
   it("returns true if rock and scissors are entered as inputs and rock wins") do
     game = Rps.new()
-    expect(game.rock_wins?("rock", "scissors")).to(eq(true))
+    expect(game.wins?("rock", "scissors")).to(eq("rock wins"))
   end
   it("returns true if scissors and paper are entered as inputs and scissors wins") do
     game = Rps.new()
-    expect(game.scissors_wins?("scissors", "paper")).to(eq(true))
+    expect(game.wins?("scissors", "paper")).to(eq("scissors wins"))
   end
   it("returns true if paper and rock are entered as inputs and paper wins") do
     game = Rps.new()
-    expect(game.paper_wins?("rock", "paper")).to(eq(true))
+    expect(game.wins?("rock", "paper")).to(eq("paper wins"))
   end
   it("returns true if rock and paper are entered as inputs and rock loses") do
     game = Rps.new()
-    expect(game.rock_loses?("rock", "paper")).to(eq(true))
+    expect(game.loses?("rock", "paper")).to(eq("rock loses"))
   end
   it("returns true if scissors and rock are entered as inputs and scissors loses") do
     game = Rps.new()
-    expect(game.scissors_loses?("scissors", "rock")).to(eq(true))
+    expect(game.loses?("scissors", "rock")).to(eq("scissors loses"))
   end
   it("returns true if scissors and paper are entered as inputs and paper loses") do
     game = Rps.new()
-    expect(game.paper_loses?("scissors", "paper")).to(eq(true))
+    expect(game.loses?("scissors", "paper")).to(eq("paper loses"))
   end
 end
