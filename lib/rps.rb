d23eed23f6ff2f12afee6f438fca7df94c45fae9
Katie Fujihara and Masen Matthews <masen@>
2018-02-07 10:22:19 -0800

    add two more tests

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,12 +1,16 @@
 class Rps
-  # def initialize(input1, input2)
-  #   @input1 = input1
-  #   @input2 = input2
-  # end
-#
-  def wins?(input1, input2)
-    @input1 = input1
-    if (input1 == "rock") && (input2 == "scissors")
+  def rock_wins?(input1, input2)
+    if (input1 == "rock"||input1 == "scissors") && (input2 == "scissors"||input2 == "rock")
+      true
+    end
+  end
+  def scissors_wins?(input1, input2)
+    if (input1 == "scissors"||input1 == "paper") && (input2 == "paper"||input2 == "scissors")
+      true
+    end
+  end
+  def paper_wins?(input1, input2)
+    if (input1 == "paper" || input1 == "rock") && (input2 == "rock" || input2 == "paper")
       true
     end
   end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -3,8 +3,16 @@ require('rps')
 require('pry')
 
 describe('#rps') do
-  it("returns true if rock is the object and scissors is the argument") do
+  it("returns true if rock and scissors are entered as inputs and rock wins") do
     game = Rps.new()
-    expect(game.wins?("rock", "scissors")).to(eq(true))
+    expect(game.rock_wins?("rock", "scissors")).to(eq(true))
+  end
+  it("returns true if scissors and paper are entered as inputs and scissors wins") do
+    game = Rps.new()
+    expect(game.scissors_wins?("scissors", "paper")).to(eq(true))
+  end
+  it("returns true if paper and rock are entered as inputs and paper wins") do
+    game = Rps.new()
+    expect(game.paper_wins?("rock", "paper")).to(eq(true))
   end
 end
